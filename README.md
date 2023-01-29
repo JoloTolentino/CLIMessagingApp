70ddc491dc032668699bf280acaa2c2d0a35ed1d
JoloTolentino <jolo.strym@gmail.com>
2023-01-28 21:12:57 -0500

    Co-authored-by: Peacebridge <Yeboah-Peacebridge-Osei@users.noreply.github.com>

--- README.md
+++ README.md
@@ -13,6 +13,12 @@ cd src
 flask --app app.py run
 ```
 
+## Use MakeFiles
+```makefile
+make se
+```
+
+
 ## Local Server will run at http://127.0.0.1:5000
 
 
--- src/app.py
+++ src/app.py
@@ -4,7 +4,9 @@ from flask_sock import Sock
 app = Flask(__name__)
 sock = Sock(app)
 
+fucntions = {
 
+} 
 
 connected_users = set()
 @sock.route('/connect') 
@@ -19,9 +21,4 @@ def handle_message(ws):
         for user in connected_users:
             user.send(text)
 
-
-
-
-
-# if __name__ == '__main__':
-#     sock.run(app, host='0.0.0.0', port=8000)
+    
